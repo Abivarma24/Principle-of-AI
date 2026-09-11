093c1110d0c7fc039239ef5b317a8a076eb205a3
Abivarma24 <abivarma.a.2024.it@rajalakshmi.edu.in>
2026-09-11 13:53:52 +0530

    Add files via upload
    exp6 uploaded

---
+++ exp6/animal.pl
@@ -0,0 +1,7 @@
+% Facts
+has_hair(tiger).
+gives_milk(tiger).
+% Rule
+mammal(X) :- has_hair(X), gives_milk(X).
+% Query
+% ?- mammal(tiger).
---
+++ exp6/simple arithmetic.pl
@@ -0,0 +1,4 @@
+% Rule
+add(X, Y, Z) :- Z is X + Y.
+% Query
+% ?- add(5, 7, Z).
